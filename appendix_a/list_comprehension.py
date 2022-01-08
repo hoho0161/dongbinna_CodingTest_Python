b004a8978fd5b6797d396413ed1d15538e03464a
hoho0161 <hoho0161@naver.com>
2022-01-08 14:13:24 +0900

    코드 추가

---
+++ appendix_a/list_comprehension.py
@@ -0,0 +1,15 @@
+#리스트를 초기화하는 방법 중 하나이다. 대괄호안에 조건문과 반복문을 넣는 방식
+
+#ex1
+array = [i for i in range(20) if i % 2 == 1]
+print(array)
+
+#ex2
+array = [i * i for i in range(1,10)]
+print(array)
+
+#ex3  2차원배열 초기화
+n = 3
+m = 4
+array = [[0] * m for _ in range(n)] # 언더바(_)는 반복을 위한 변수의 값을 무시하고자 할 때
+print(array)
